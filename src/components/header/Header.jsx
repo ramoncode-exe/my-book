c95a1277f8d155e834a3b9131543a4129238147f
ramoncode-exe <ramonpatrick0906@gmail.com>
2025-05-29 10:35:08 +0100

    News Updates on links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,8 +10,8 @@ function Header(){
             </div>
             <div>
                 <ul className="nav-links">
-                    <motion.button whileHover={{ scale: 1.1 }}  onClick={window.open("https://www.youtube.com/@ramonaqui")} className="btnChannel">Ver Canal</motion.button>
-                    <motion.button whileHover={{ scale: 1.1 }}  onClick={window.open("https://www.linkedin.com/in/ramonprog/")} className="btnContact">Contato</motion.button>
+                    <motion.button whileHover={{ scale: 1.1 }} onClick={() => window.open("https://www.youtube.com/@ramonaqui", "_blank")} className="btnChannel">Ver Canal</motion.button>
+                    <motion.button whileHover={{ scale: 1.1 }} onClick={() => window.open("https://www.linkedin.com/in/ramonprog/", "_blank")} className="btnContact">Contato</motion.button>
                 </ul>
             </div>
         </div>
