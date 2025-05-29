d2e74362fa946ea3463b6371888688a61245ea8d
ramoncode-exe <ramonpatrick0906@gmail.com>
2025-05-29 09:44:08 +0100

    New Section - Benefits, Box Shadow and Responsive Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home.jsx";
+import Benefits from "./components/benefits/Benefits.jsx";
 
 function App(){
   return(
     <div>
       <Header />
       <Home />
+      <Benefits />
     </div> 
   );
 }
